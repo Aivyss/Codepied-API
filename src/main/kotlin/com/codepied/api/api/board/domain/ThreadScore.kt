964a823c3c39f0a4b6b657482dfea6406a6d5ba9
junyeong.jo <whwnsdud2024@naver.com>
2023-02-13 11:54:33 +0900

    CA-20 [API] 게시판 Class Diagram 설계 및 Entity 생성

--- src/main/kotlin/com/codepied/api/api/board/domain/ThreadScore.kt
+++ src/main/kotlin/com/codepied/api/api/board/domain/ThreadScore.kt
@@ -4,6 +4,7 @@ import com.codepied.api.api.domain.Audit
 import com.codepied.api.user.domain.User
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import javax.persistence.*
+import com.codepied.api.api.board.domain.Thread as Thread;
 
 /**
  * @author Nairobi
@@ -15,16 +16,16 @@ import javax.persistence.*
 @EntityListeners(AuditingEntityListener::class)
 class ThreadScore(
         @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        @Column(name = "THREAD_KEY")
+        @Column(name = "THREAD_KEY", nullable = false)
         val id: Long,
+
+        @OneToOne(fetch = FetchType.LAZY)
+        @MapsId
+        @JoinColumn(name = "THREAD_KEY", nullable = false, updatable = false)
+        val thread: Thread,
 
         @Column(name = "SCORE", nullable = false, updatable = true)
         var score: Long,
-
-        @ManyToOne(fetch = FetchType.LAZY)
-        @JoinColumn(name = "THREAD_KEY", nullable = false, updatable = false)
-        val thread: Thread,
 
         @Embedded
         val audit: Audit = Audit(),
