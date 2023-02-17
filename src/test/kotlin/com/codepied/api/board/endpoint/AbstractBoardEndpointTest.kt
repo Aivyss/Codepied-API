10d39f309e43a2d594bc98fc859625c8b67e44ec
junyeong.jo <whwnsdud2024@naver.com>
2023-02-17 12:13:53 +0900

    CA-42 [API] 게시판 생성 구현

---
+++ src/test/kotlin/com/codepied/api/board/endpoint/AbstractBoardEndpointTest.kt
@@ -0,0 +1,86 @@
+package com.codepied.api.user.endpoint
+
+import com.codepied.api.api.config.I18nConfig
+import com.codepied.api.api.exception.ExceptionController
+import com.codepied.api.api.http.RequestContext
+import com.codepied.api.api.locale.LocaleChangeFilter
+import com.codepied.api.api.security.JwtAuthFilter
+import com.codepied.api.api.security.application.EmailLoginService
+import com.codepied.api.api.security.application.JwtService
+import com.codepied.api.board.application.BoardService
+import com.codepied.api.board.endpoint.BoardController
+import com.codepied.api.user.application.UserIntegrationService
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration
+import org.springframework.boot.autoconfigure.context.MessageSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.FilterType
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext
+import org.springframework.restdocs.RestDocumentationContextProvider
+import org.springframework.restdocs.RestDocumentationExtension
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentationConfigurer
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
+
+/**
+ * Spring Rest Doc 적용 및 mockMvc Setup을 위한 추상클래스
+ *
+ * @author Aivyss
+ * @since 2022/12/22
+ */
+@ActiveProfiles("test")
+@ImportAutoConfiguration(MessageSourceAutoConfiguration::class)
+@MockBean(JpaMetamodelMappingContext::class)
+@ExtendWith(value = [RestDocumentationExtension::class, SpringExtension::class])
+@WebMvcTest(
+        value = [
+            ExceptionController::class,
+            BoardController::class,
+            RequestContext::class,
+            MockMvcRestDocumentationConfigurer::class,
+            RestDocumentationContextProvider::class,
+            I18nConfig::class,
+        ],
+        excludeFilters = [ComponentScan.Filter(
+                type = FilterType.ASSIGNABLE_TYPE,
+                classes = [LocaleChangeFilter::class, JwtAuthFilter::class])],
+        excludeAutoConfiguration = [
+            SecurityAutoConfiguration::class,
+        ]
+)
+abstract class AbstractBoardEndpointTest(val uri: String) {
+    @MockBean
+    protected lateinit var boardService: BoardService
+
+    @MockBean
+    protected lateinit var jwtService: JwtService
+
+    @Autowired
+    private lateinit var webApplicationContext: WebApplicationContext
+
+    private lateinit var restDocumentation: RestDocumentationContextProvider
+
+    protected lateinit var mockMvc: MockMvc
+
+    protected val accessToken = "fjoaiwefasw.fkjasfoawef.fjioaefjawe"
+    protected val refreshToken = "fiawjeofaswesf.viaowefjwe.ckZXJCOasd"
+
+    @BeforeEach
+    fun initRestDocumentation(restDocumentation: RestDocumentationContextProvider) {
+        this.restDocumentation = restDocumentation
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .apply<DefaultMockMvcBuilder>(documentationConfiguration(restDocumentation))
+                .build()
+    }
+}
--- src/test/kotlin/com/codepied/api/board/endpoint/BoardControllerTest.kt
+++ src/test/kotlin/com/codepied/api/board/endpoint/BoardControllerTest.kt
@@ -1,10 +1,61 @@
 package com.codepied.api.board.endpoint
 
+import com.codepied.api.board.application.BoardService
+import com.codepied.api.board.dto.BoardCreate
+import com.codepied.api.test.DocumentEnum
+import com.codepied.api.test.RestDocStore
+import com.codepied.api.user.endpoint.AbstractBoardEndpointTest
+import com.codepied.api.user.endpoint.AbstractUserEndpointTest
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doNothing
+import org.mockito.kotlin.doReturn
+import org.springframework.http.MediaType
+import org.springframework.restdocs.headers.HeaderDocumentation
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation
+import org.springframework.restdocs.payload.PayloadDocumentation
+import org.springframework.restdocs.request.RequestDocumentation
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+
 /**
  *
  *
  * @author junyeong.jo .
  * @since 2023-02-15
  */
-class BoardControllerTest {
+class BoardControllerTest : AbstractBoardEndpointTest("/api/board") {
+
+    @Test
+    fun `게시판 생성 성공 ` () {
+        // * given
+        doNothing(
+        ).`when`(boardService).createBoard(any())
+
+        // * when
+        val perform = mockMvc.perform(
+            MockMvcRequestBuilders.post(uri)
+                .header("Authorization", "Bearer $accessToken")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content("""
+                    { 
+                        "name": "name"
+                    }
+            """.trimMargin()))
+
+        // * then
+        perform.andExpect(status().isCreated)
+            MockMvcRestDocumentation.document(DocumentEnum.BOARD_CREATE.name,
+            HeaderDocumentation.requestHeaders(
+                HeaderDocumentation.headerWithName("Authorization").description("Bearer \${accessToken}"),
+            ),
+            PayloadDocumentation.requestFields(
+                PayloadDocumentation.fieldWithPath("name").type("String").description("게시판 이름 / 공백불가 / 특수문자 불가"),
+            ),
+        )
+
+
+    }
 }
